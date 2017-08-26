46e381de06740b22c04d5b8a5fd0108d3de0f929
HexEdit <hexedit@ifproject.ru>
2017-08-27 00:14:01 +0300

    Added support for commands

--- app/mydiscordimpl.py
+++ app/mydiscordimpl.py
@@ -1,12 +1,13 @@
 from . import MyDiscord
 from configparser import ConfigParser, NoSectionError, NoOptionError
-from discord import opus, ChannelType
+from discord import opus, ChannelType, InvalidArgument
 from yandex_speech import TTS
 from random import randint
-from asyncio import sleep
+import asyncio
 from tempfile import gettempdir
 import codecs
 import os.path
+import re
 
 tts_voices = [
     'jane',
@@ -21,7 +22,7 @@ if not opus.is_loaded():
     opus.load_opus('voice')
 
 config = ConfigParser()
-config.read_file(codecs.open('config.ini','r','utf-8'))
+config.read_file(codecs.open('config.ini', 'r', 'utf-8'))
 
 client = MyDiscord()
 try:
@@ -95,11 +96,40 @@ def on_ready():
         pass
 
 
+@asyncio.coroutine
+def process_command11(cmd, arg, message):
+    print("Got command \033[01m{}\033[00m from \033[01m{}\033[00m with argument \033[01m{}\033[00m"
+          .format(cmd, message.author.name, arg))
+    if cmd == 'move to' or cmd == 'go to' or cmd == 'пиздуй в':
+        try:
+            global voice
+            ch = client.get_channel(arg)
+            if ch is None or ch.type is not ChannelType.voice:
+                raise InvalidArgument('Not a voice channel')
+            print("\033[01m{}\033[00m requested me to move to \033[01m{channel.name}\033[00m"
+                  " on \033[01m{channel.server.name}\033[00m"
+                  .format(message.author.name, channel=ch))
+            if voice:
+                yield from voice.disconnect()
+            voice = yield from client.join_voice_channel(ch)
+        except InvalidArgument as e:
+            yield from client.send_message(message.channel, '\n'.join(e.args))
+            pass
+
+
 @client.async_event
 def on_message(message):
     global player
-    toplay = None
-    if voice.is_connected() and player is None:
+    cmd_ptrn = '^\s*([\w\s]+)\s*:\s*(.*)\s*$'
+    if message.author == client.user:
+        return
+    m = re.findall(cmd_ptrn, message.content)
+    if m and message.server is None:
+        cmd = m[0][0].lower()
+        arg = m[0][1]
+        yield from process_command11(cmd, arg, message)
+    elif voice.is_connected() and player is None:
+        to_play = None
         if message.content.lower() in voice_messages:
             if message.server is None:
                 message_author = message.author.name
@@ -110,7 +140,7 @@ def on_message(message):
             print("Playing message for \033[01m{}\033[00m at volume \033[01m{}\033[00m"
                   " by \033[01m{}\033[00m at \033[01m{}\033[00m"
                   .format(message.content, voice_volume, message_author, message_channel))
-            toplay = voice_messages[message.content.lower()]
+            to_play = voice_messages[message.content.lower()]
         elif message.server is None:
             print("Retrieving TTS for \033[01m{}\033[00m by \033[01m{}\033[00m"
                   .format(message.content, message.author.name))
@@ -119,10 +149,10 @@ def on_message(message):
                 key = config.get('tts', 'api key')
                 tts = TTS(tts_voice, 'opus', key, lang='ru_RU', emotion='neutral')
                 tts.generate(message.content)
-                toplay = tts.save(os.path.join(gettempdir(), "tts.opus"))
+                to_play = tts.save(os.path.join(gettempdir(), "tts.opus"))
             except (NoSectionError, NoOptionError):
                 pass
-        play_file(toplay)
+        play_file(to_play)
 
 
 @client.async_event
@@ -136,7 +166,7 @@ def on_voice_state_update(before, after):
             except NoOptionError:
                 pass
         elif after.voice_channel == voice.channel:
-            yield from sleep(greeting_delay)
+            yield from asyncio.sleep(greeting_delay)
             if after.id in greetings.keys():
                 play_file(greetings[after.id])
             elif 'default' in greetings.keys():
