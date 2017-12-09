96b810ea15112c40636ce3711bf0507e5039ea91
HexEdit <hexedit@ifproject.ru>
2017-12-09 13:57:15 +0300

    Stop on SIGTERM and SIGINT

--- app/mydiscord.py
+++ app/mydiscord.py
@@ -1,10 +1,21 @@
 import discord
+import asyncio
 
 
 class MyDiscord(discord.Client):
     def __init__(self):
         super().__init__()
         self._token = ""
+        self._stop = False
+
+    @asyncio.coroutine
+    def _wait_for_stop(self):
+        while True:
+            if self._stop:
+                yield from self.logout()
+                print("Logged out")
+                break
+            yield from asyncio.sleep(1)
 
     @property
     def token(self):
@@ -15,4 +26,8 @@ class MyDiscord(discord.Client):
         self._token = val
 
     def run(self):
+        self.loop.create_task(self._wait_for_stop())
         super().run(self._token)
+
+    def stop(self):
+        self._stop = True
--- run.py
+++ run.py
@@ -1,10 +1,21 @@
 #!bin/python
 
 from app import app
+import signal
+
+
+def finish(sig, frame):
+    del sig, frame
+    print("Terminate signal received. Stopping...")
+    app.stop()
+
 
 if __name__ == '__main__':
+    signal.signal(signal.SIGTERM, finish)
+    signal.signal(signal.SIGINT, finish)
     while True:
         try:
             app.run()
         finally:
+            print("Graceful shutdown, exiting")
             break
